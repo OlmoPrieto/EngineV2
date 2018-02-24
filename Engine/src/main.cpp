00b3caa695705bf35d64ba173863e18444d1dbe4
OlmoPrieto <prieto.sanchez.olmo@gmail.com>
2018-02-24 11:29:24 +0100

    Made OpenGL test program to work in Linux

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -154,9 +154,9 @@ int main() {
     return 1;
   }
 
-  // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   #ifdef __PLATFORM_MACOSX__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -310,7 +310,7 @@ int main() {
   glUseProgram(program_id);
   glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)p.matrix);
 
-  unsigned int indices[6] = { 3, 0, 1, 1, 2, 3 };
+  unsigned int indices[6] = { 0, 1, 2, 3, 4, 5 };
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
 
